cecbf5add5ab52059fbedc055b26599ef1b5b4b6
Marija Najdova <manajdov@microsoft.com>
2018-07-23 13:46:47 +0000

    -reverted changes in echo dialog

--- Dialogs/EchoDialog.cs
+++ Dialogs/EchoDialog.cs
@@ -33,7 +33,7 @@ namespace Microsoft.Bot.Sample.SimpleEchoBot
             }
             else
             {
-                await context.PostAsync($"{this.count++}: You were saying{message.Text}");
+                await context.PostAsync($"{this.count++}: You said {message.Text}");
                 context.Wait(MessageReceivedAsync);
             }
         }
